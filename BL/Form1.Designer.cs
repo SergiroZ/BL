f8f89485d4f7279542167bae56bedda3af9156b1
Sergiro Z <zujonok@hotmail.com>
2018-05-12 14:22:33 +0300

    BL

--- BL/Form1.Designer.cs
+++ BL/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabBooks = new System.Windows.Forms.TabPage();
+            this.tabAuthors = new System.Windows.Forms.TabPage();
+            this.tabPablishers = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabBooks.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabBooks);
+            this.tabControl1.Controls.Add(this.tabAuthors);
+            this.tabControl1.Controls.Add(this.tabPablishers);
+            this.tabControl1.Location = new System.Drawing.Point(23, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 400);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabBooks
+            // 
+            this.tabBooks.Controls.Add(this.dataGridView1);
+            this.tabBooks.Location = new System.Drawing.Point(4, 22);
+            this.tabBooks.Name = "tabBooks";
+            this.tabBooks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBooks.Size = new System.Drawing.Size(752, 374);
+            this.tabBooks.TabIndex = 0;
+            this.tabBooks.Text = "Books";
+            this.tabBooks.UseVisualStyleBackColor = true;
+            // 
+            // tabAuthors
+            // 
+            this.tabAuthors.Location = new System.Drawing.Point(4, 22);
+            this.tabAuthors.Name = "tabAuthors";
+            this.tabAuthors.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAuthors.Size = new System.Drawing.Size(752, 374);
+            this.tabAuthors.TabIndex = 1;
+            this.tabAuthors.Text = "Authors";
+            this.tabAuthors.UseVisualStyleBackColor = true;
+            // 
+            // tabPablishers
+            // 
+            this.tabPablishers.Location = new System.Drawing.Point(4, 22);
+            this.tabPablishers.Name = "tabPablishers";
+            this.tabPablishers.Size = new System.Drawing.Size(752, 374);
+            this.tabPablishers.TabIndex = 2;
+            this.tabPablishers.Text = "Pablishers";
+            this.tabPablishers.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(746, 368);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabBooks.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabBooks;
+        private System.Windows.Forms.TabPage tabAuthors;
+        private System.Windows.Forms.TabPage tabPablishers;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
